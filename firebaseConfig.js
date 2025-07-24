af373e15b9fc49be483c123b725a6d5355873f51
aliaa1h <aliaalhosani588053@gmail.com>
2025-07-24 21:38:57 +0400

    Create firebaseConfig.js

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+// firebaseConfig.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyCJgJ3S5Qgp9BBC6G8zEwuywzirkUjQHPo",
+  authDomain: "fir-config-eaec9.firebaseapp.com",
+  projectId: "fir-config-eaec9",
+  storageBucket: "fir-config-eaec9.firebasestorage.app",
+  messagingSenderId: "254127270989",
+  appId: "1:254127270989:web:d000a18b2b7e1ab16f0004",
+  measurementId: "G-NJYQ6LL9N9"
+};
